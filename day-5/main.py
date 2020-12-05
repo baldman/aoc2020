20eab5d5c0a21c1e318e61eae9c08718373b3617
Tomas <tomas@plesek.cz>
2020-12-05 15:39:22 +0100

    Slight cleanup

--- day-5/main.py
+++ day-5/main.py
@@ -1,12 +1,10 @@
 def run():
     def find_position(vector, pos_range):
-        result = 0
         for current in vector:
             diff = (pos_range[1] - pos_range[0]) // 2
 
-            if diff == 0: # We're at the end
-                result = pos_range[0] if current == 'L' else pos_range[1]
-                return result
+            if diff == 0:  # We're at the end
+                return pos_range[0] if current == 'L' else pos_range[1]
 
             if current == 'L':
                 pos_range = pos_range[0], pos_range[0] + diff
@@ -24,21 +22,18 @@ def run():
             row = find_position(line[:7].replace('F', 'L').replace('B', 'R'), (0, 127))
             seat = find_position(line[7:], (0, 7))
             seat_id = row * 8 + seat
-
             ids.append(seat_id)
+
             if seat_id > highest:
                 highest = seat_id
 
     print(f">>> Highest seat id {highest}")
+
     prev = None
     for x in sorted(ids):
-        if prev is None:
-            prev = x
-            continue
-
-        if prev+1 != x:
-            print(f"My seat is {prev+1}")
-
+        if prev is not None and prev+1 != x:
+            print(f">>> My seat is {prev+1}")
+            return
         prev = x
 
 
